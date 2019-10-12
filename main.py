7f676afa9a4c9577d8d356aa77e97f03c0ec2bdd
coolabyla <akozadeeva@mail.ru>
2019-10-12 16:10:48 +0700

    change

--- main.py
+++ main.py
@@ -4,11 +4,12 @@
 #               Alexandra Kozadeeva ()
 #               Anzhelika Kurepova ()
 
-import turtle
-
 def square(x,y,a,color,angle)
     #TODO: (Alexandra) square
-    #
+    import turtle
+    turtle.forward(200)
+    turtle.right(90)
+    turtle.forward(200)
     pass
 
 def triangle(x,y,,color)
