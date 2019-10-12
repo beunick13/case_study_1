7701c3f5c4107fec1118856e09e9b56af325bdac
coolabyla <akozadeeva@mail.ru>
2019-10-12 16:50:33 +0700

    change

--- main.py
+++ main.py
@@ -3,16 +3,17 @@
 # Developers:   Nikita Beushev ()
 #               Alexandra Kozadeeva ()
 #               Anzhelika Kurepova ()
-
+import turtle
 def square(x,y,a,color,angle):
     #TODO: (Alexandra) square
-    import turtle
+    turtle.showturtle()
+    turtle.down()
     turtle.forward(200)
     turtle.right(90)
     turtle.forward(200)
     pass
 
-def triangle(x,y,,color):
+def triangle(x,y,a,color):
     #TODO: (Anzhelika) triangle
     #ghf
     ddhh
