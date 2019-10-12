40730bea76ddb65a7d8370c3aeaf7c74fd03430a
beunick13 <beushevnv@gmail.com>
2019-10-12 16:06:56 +0700

    Initial commit

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # Case 1
 
-# Developers:   Nikita Beushev
-#               Alexandra Kozadeeva
-#               Anzhelika Kurepova
+# Developers:   Nikita Beushev ()
+#               Alexandra Kozadeeva ()
+#               Anzhelika Kurepova ()
 
 import turtle
 
-def square(x,y,a,color)
-    #TODO
+def square(x,y,a,color,angle)
+    #TODO: (Alexandra) square
+    #
     pass
 
-def triangle()
+def triangle(x,y,,color)
+    #TODO: (Anzhelika) triangle
+    #
     pass
 
 def rhombus()
+    #TODO: (Nikita) rhombus
+    #
     pass
