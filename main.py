78cfee5e2962e81d786a5b5536f02014f875e348
coolabyla <akozadeeva@mail.ru>
2019-10-18 22:51:56 +0700

    2 фигуры в левом нижнем углу

--- main.py
+++ main.py
@@ -27,7 +27,25 @@ def square(x, y, a, angle, line_color, fill_color):
     turtle.right(90)
     turtle.end_fill()
     pass
-
+def rectangle(x, y, a, b, angle, line_color, fill_color):
+    #TODO: (Alexandra) square
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(angle)
+    turtle.pencolor(line_color)
+    turtle.fillcolor(fill_color)
+    turtle.down()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(b)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(b)
+    turtle.right(90)
+    turtle.end_fill()
+    pass
 
 def triangle(x, y, size, angle, line_color, fill_color):
     #TODO: (Anzhelika) triangle
@@ -120,42 +138,36 @@ def picture_4():
 
 
 def picture_5():
-    # 2x2
-    # -150, -150
-    # Anzhelika
     pass
-
 
 def picture_6():
-    # 2x3
-    # 150, -150
-    # Anzhelika
+    circle (-300, -250, 100, 'white', 'red')
+    circle(-310, -240, 80, 'white', 'white')
+    circle (-320, -80, 50, 'white', 'red')
+    circle(-330, -70, 30, 'white', 'white')
+    rectangle(-410, -167, 53, 100, 90, 'white', 'black')
+    line(-370, -120, 20, 0, 'pink')
+    line(-370, -140, 20, 0, 'pink')
+    line(-370, -160, 20, 0, 'pink')
+    line(-370, -160, 40, 90, 'pink')
+    line(-400, -160, 40, 90, 'pink')
+    line(-400, -140, 14, 45, 'pink')
+    line(-400, -120, 14, 315, 'pink')
+    line(-330, -160, 40, 90, 'pink')
+    line(-330, -140, 14, 45, 'pink')
+    line(-330, -120, 14, 315, 'pink')
+    rhombus(1, 1, 10, 5, 0, 'white', 'black')
+    triangle(-200, -50, 50, 45, 'white', 'purple')
+    triangle(-200, -120, 70, 90, 'white', 'lightblue')
+    triangle(-128, -190, 97, 135, 'white', 'red')
+    triangle(-200, -121, 96, 315, 'white', 'yellow')
+    square(-165, -225, 50, 45, 'white', 'orange')
+    triangle(-95, -295, 50, 135, 'white', 'purple')
+    rhombus(-235, -290, 50, 70, 45, 'white', 'green')
     pass
-
-
-def picture_7():
-    # 3x1
-    # -450, -450
-    # Alexandra
-    pass
-
-
-def picture_8():
-    # 3x2
-    # -150, -450
-    # Alexandra
-    pass
-
-
-def picture_9():
-    # 3x3
-    # 150, -450
-    # Alexandra
-    pass
-
 
 def main():
-    turtle.setup(900, 900)
+
     turtle.speed(0)
     turtle.hideturtle()
     picture_1()
@@ -164,9 +176,7 @@ def main():
     picture_4()
     picture_5()
     picture_6()
-    picture_7()
-    picture_8()
-    picture_9()
+
     turtle.mainloop()
 
 
